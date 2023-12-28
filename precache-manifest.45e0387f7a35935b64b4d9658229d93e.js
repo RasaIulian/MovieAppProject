a82397c89dddafd8996b506c0f341803acafc2a0
RasaIulian <ilie.rasa@gmail.com>
2023-12-28 17:17:19 +0200

    Updates

---
+++ precache-manifest.45e0387f7a35935b64b4d9658229d93e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8cc6ef260268d438c1a5",
+    "url": "/movieappproject/static/css/main.47c44def.chunk.css"
+  },
+  {
+    "revision": "8cc6ef260268d438c1a5",
+    "url": "/movieappproject/static/js/main.89d1fd4d.chunk.js"
+  },
+  {
+    "revision": "7b5d59cd13d3c23520f6",
+    "url": "/movieappproject/static/js/runtime~main.637f8497.js"
+  },
+  {
+    "revision": "c0c989dfcde212253672",
+    "url": "/movieappproject/static/js/2.ce615b3d.chunk.js"
+  },
+  {
+    "revision": "50934b123c31cfcd1c96d173d840086c",
+    "url": "/movieappproject/index.html"
+  }
+];
