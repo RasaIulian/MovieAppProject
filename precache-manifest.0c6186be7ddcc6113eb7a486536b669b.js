fcdbb27bab51f51d31ece282ca445043bf490b9e
RasaIulian <ilie.rasa@gmail.com>
2024-12-10 17:08:27 +0200

    Updates

--- precache-manifest.0c6186be7ddcc6113eb7a486536b669b.js
+++ precache-manifest.0c6186be7ddcc6113eb7a486536b669b.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "05f10924812aba80c29c",
+    "revision": "1751429317317a330007",
     "url": "/MovieAppProject/static/css/main.1ed4c54e.chunk.css"
   },
   {
-    "revision": "05f10924812aba80c29c",
-    "url": "/MovieAppProject/static/js/main.2a9295ab.chunk.js"
+    "revision": "1751429317317a330007",
+    "url": "/MovieAppProject/static/js/main.014340b1.chunk.js"
   },
   {
     "revision": "bb8ef6725de1262ab655",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/MovieAppProject/static/js/2.a783ef85.chunk.js"
   },
   {
-    "revision": "21f60a4c81722afe0a8d7d0834daa0f0",
+    "revision": "26b05da16e328cf321e738f439e983a9",
     "url": "/MovieAppProject/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/MovieAppProject/precache-manifest.934e3efb7389804ff8cfa1cec8a802ee.js"
+  "/MovieAppProject/precache-manifest.0c6186be7ddcc6113eb7a486536b669b.js"
 );
 
 workbox.clientsClaim();
